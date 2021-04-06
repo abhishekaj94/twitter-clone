d666e33fcc489a21e8ae8e777f106002735f4702
abhishekaj94 <abhishekaj94@gmail.com>
2021-04-06 15:55:45 +0530

    Like tweet functionality

--- src/Post.js
+++ src/Post.js
@@ -3,10 +3,30 @@ import './Post.css';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import RepeatIcon from '@material-ui/icons/Repeat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import PublishIcon from '@material-ui/icons/Publish';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Post = ({ tweet, displayname, handle, timestamp, postText, postImage, avatar }) => {
 
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeTweet = (tweetId) => {
+    const postParams = {
+      tweetId : tweetId
+    }
+    axios.post('http://localhost:4000/tweets/like',postParams).then(response=>{
+      var responseData = response.data
+      // console.log(response)
+      if(responseData.Status ==="OK"){
+        setIsLiked(true)
+      }
+      else{
+        alert('Failed to make request')
+      }
+    })
+  }
 
   return ( 
     <div className="post">
@@ -25,9 +45,10 @@ const Post = ({ tweet, displayname, handle, timestamp, postText, postImage, avat
           {/* <img src="https://learn.g2.com/hs-fs/hubfs/plan%20gif%20marketing%20strategy.gif?width=500&name=plan%20gif%20marketing%20strategy.gif" alt="Oops"/> */}
         </div>
         <div className="post__body__actionButtons">
-          <ChatBubbleOutlineIcon fontSize="small" />{tweet.detail.comments.length}
-          <RepeatIcon fontSize="small"/>{tweet.retweets}
-          <FavoriteBorderIcon fontSize="small"/>{tweet.likes}
+          <ChatBubbleOutlineIcon fontSize="small" style={{ color: 'green' }}/><span>{tweet.detail.comments.length}</span>
+          <RepeatIcon fontSize="small" style={{ color: 'blue' }}/><span>{tweet.retweets}</span>
+          {isLiked && <FavoriteIcon fontSize="small" style={{ color: 'red' }} onClick={() => setIsLiked(false)}/> }
+          {!isLiked && <FavoriteBorderIcon fontSize="small" style={{ color: 'red' }} onClick={() => likeTweet(tweet.tweetid) } />}<span>{tweet.likes}</span>
           <PublishIcon fontSize="small"/>
         </div>
       </div>
